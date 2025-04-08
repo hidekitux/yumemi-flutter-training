b73f744b66bff5c3eabc1eee2aeae77a7422296a
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-08 16:23:34 +0900

    feat: Add weather icons and initial UI setup

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_training/presentation/components/temperature_indicator.dart';
 import 'package:flutter_training/presentation/components/weather_action_button.dart';
 
@@ -14,7 +15,10 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               const Spacer(),
-              const AspectRatio(aspectRatio: 1, child: Placeholder()),
+              AspectRatio(
+                aspectRatio: 1,
+                child: SvgPicture.asset('assets/images/sunny.svg'),
+              ),
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 16),
                 child: Row(
@@ -29,7 +33,9 @@ class HomeScreen extends StatelessWidget {
                   padding: const EdgeInsets.only(top: 80),
                   child: Row(
                     children: [
+                      // TODO: Closeボタンを押したときの動作を実装する
                       WeatherActionButton(label: 'Close', onPressed: () {}),
+                      // TODO: Reloadボタンを押したときの動作を実装する
                       WeatherActionButton(label: 'Reload', onPressed: () {}),
                     ],
                   ),
