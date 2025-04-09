3c21144b090926589acce581dbada3e96b4deec7
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-09 13:01:19 +0900

    feat: Add ReloadWeatherUseCase

---
+++ lib/application/usecases/reload_weather_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_training/domain/common/usecase.dart';
+import 'package:flutter_training/domain/weather/entities/weather_condition_entity.dart';
+import 'package:flutter_training/infrastructure/weather/repositories/weather_repository.dart';
+
+class ReloadWeatherUseCase implements UseCase<WeatherCondition> {
+  ReloadWeatherUseCase(this._weatherRepository);
+
+  final WeatherRepository _weatherRepository;
+
+  @override
+  void execute(
+    UseCaseSuccessCallback<WeatherCondition> onSuccess,
+    UseCaseErrorCallback onError,
+  ) {
+    try {
+      final weatherCondition = WeatherCondition.fromString(
+        _weatherRepository.getSimpleWeather(),
+      );
+      return onSuccess(weatherCondition);
+    } on Exception catch (error) {
+      return onError(error);
+    }
+  }
+}
