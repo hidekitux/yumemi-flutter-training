a79184d3fd93e06200bb0b613b6fb992f02ac05a
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-10 11:21:30 +0900

    feat: Add common error messages and weather error messages

---
+++ lib/domain/weather/constants/weather_error_message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_training/domain/common/constants/common_error_messages.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+enum WeatherErrorMessages { invalidParameter, unknown }
+
+extension YumemiWeatherErrorExt on YumemiWeatherError {
+  String get message {
+    return switch (this) {
+      YumemiWeatherError.invalidParameter =>
+        ErrorMessages.invalidParameter.message,
+      YumemiWeatherError.unknown => ErrorMessages.unknown.message,
+    };
+  }
+}
