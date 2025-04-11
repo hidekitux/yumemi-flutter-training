52eb44770bc94161f13984265ad4b3acdb8fd292
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-11 12:23:39 +0900

    feat: Implement fetchWeather to replace fetchThrowsWeather

--- lib/infrastructure/weather/api/weather_service.dart
+++ lib/infrastructure/weather/api/weather_service.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter_training/infrastructure/weather/api/weather_request.dart';
+import 'package:flutter_training/infrastructure/weather/api/weather_response.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherService {
@@ -5,8 +9,17 @@ class WeatherService {
 
   final YumemiWeather _yumemiWeather;
 
-  String fetchThrowsWeather() {
+  WeatherResponse fetchWeather() {
     // TODO: 仮の値を変更する
-    return _yumemiWeather.fetchThrowsWeather('tokyo');
+    final requestData = WeatherRequest(area: 'tokyo', date: DateTime.now());
+    final requestString = jsonEncode(requestData.toJson());
+    final jsonString = _yumemiWeather.fetchWeather(requestString);
+    final jsonData = jsonDecode(jsonString);
+
+    if (jsonData is Map<String, dynamic>) {
+      return WeatherResponse.fromJson(jsonData);
+    } else {
+      throw const FormatException('Invalid weather data');
+    }
   }
 }
