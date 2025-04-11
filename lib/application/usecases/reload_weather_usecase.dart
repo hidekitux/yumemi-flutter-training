580676cfc21a80b3004d2e45eb14b4cd82a68bc7
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-11 12:25:14 +0900

    feat: Change return type of ReloadWeatherUseCase to WeatherInfo

--- lib/application/usecases/reload_weather_usecase.dart
+++ lib/application/usecases/reload_weather_usecase.dart
@@ -1,24 +1,24 @@
 import 'package:flutter_training/domain/common/interfaces/result.dart';
 import 'package:flutter_training/domain/common/interfaces/usecase.dart';
-import 'package:flutter_training/domain/weather/entities/weather_condition_entity.dart';
+import 'package:flutter_training/domain/weather/entities/weather_info_entity.dart';
 import 'package:flutter_training/infrastructure/weather/repositories/weather_repository.dart';
 
-class ReloadWeatherUseCase implements UseCase<WeatherCondition> {
+class ReloadWeatherUseCase implements UseCase<WeatherInfo> {
   ReloadWeatherUseCase(this._weatherRepository);
 
   final WeatherRepository _weatherRepository;
 
   @override
   void execute({
-    required UseCaseSuccessCallback<WeatherCondition> onSuccess,
+    required UseCaseSuccessCallback<WeatherInfo> onSuccess,
     required UseCaseErrorCallback onError,
   }) {
-    final weatherCondition = _weatherRepository.getThrowsWeather();
-    switch (weatherCondition) {
-      case Success<WeatherCondition>():
-        onSuccess(weatherCondition.value);
-      case Failure<WeatherCondition>():
-        onError(weatherCondition.message);
+    final weatherInfo = _weatherRepository.getWeather();
+    switch (weatherInfo) {
+      case Success<WeatherInfo>():
+        onSuccess(weatherInfo.value);
+      case Failure<WeatherInfo>():
+        onError(weatherInfo.message);
     }
   }
 }
