def3f78394bfe784f88551557fbf677ba95e0ffe
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-11 12:24:12 +0900

    feat: Implement getWeather to replace getThrowsWeather

--- lib/infrastructure/weather/repositories/weather_repository.dart
+++ lib/infrastructure/weather/repositories/weather_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_training/domain/common/constants/common_error_messages.dart';
 import 'package:flutter_training/domain/common/interfaces/result.dart';
 import 'package:flutter_training/domain/weather/constants/weather_error_message.dart';
-import 'package:flutter_training/domain/weather/entities/weather_condition_entity.dart';
+import 'package:flutter_training/domain/weather/entities/weather_info_entity.dart';
 import 'package:flutter_training/infrastructure/weather/api/weather_service.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
@@ -10,11 +10,10 @@ class WeatherRepository {
 
   final WeatherService _weatherService;
 
-  Result<WeatherCondition> getThrowsWeather() {
+  Result<WeatherInfo> getWeather() {
     try {
-      final result = _weatherService.fetchThrowsWeather();
-      final weatherCondition = WeatherCondition.fromString(result);
-      return Success(weatherCondition);
+      final response = _weatherService.fetchWeather();
+      return Success(response.toInfo());
     } on YumemiWeatherError catch (error) {
       return Failure(error.message);
     } on Exception {
