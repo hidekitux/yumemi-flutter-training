afb4c56af4518b237d303e8f9bcf9ca75eb61a4b
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-14 14:09:37 +0900

    refactor: Rename weather_error_message.dart to weather_error_messages.dart

--- lib/infrastructure/weather/repositories/weather_repository.dart
+++ lib/infrastructure/weather/repositories/weather_repository.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_training/domain/common/constants/common_error_messages.dart';
 import 'package:flutter_training/domain/common/interfaces/result.dart';
-import 'package:flutter_training/domain/weather/constants/weather_error_message.dart';
+import 'package:flutter_training/domain/weather/constants/weather_error_messages.dart';
 import 'package:flutter_training/domain/weather/entities/weather_condition_entity.dart';
 import 'package:flutter_training/infrastructure/weather/api/weather_service.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
