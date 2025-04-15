590886b3b0292a8e4ac6c0e36cf2cb802481b77a
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-15 15:53:53 +0900

    feat: Integrate WeatherInfo and WeatherResponse

--- lib/domain/weather/entities/weather_info_entity.dart
+++ lib/domain/weather/entities/weather_info_entity.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_training/domain/weather/entities/weather_condition_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'weather_info_entity.g.dart';
+
+@JsonSerializable()
 class WeatherInfo {
   const WeatherInfo({
     required this.weatherCondition,
@@ -8,8 +12,13 @@ class WeatherInfo {
     required this.date,
   });
 
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) =>
+      _$WeatherInfoFromJson(json);
+
   final WeatherCondition weatherCondition;
   final int maxTemperature;
   final int minTemperature;
   final DateTime date;
+
+  Map<String, dynamic> toJson() => _$WeatherInfoToJson(this);
 }
