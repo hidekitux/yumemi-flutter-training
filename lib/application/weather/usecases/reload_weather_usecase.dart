65ca5a31445018bd72ac79e97912f447ce6dc629
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-16 15:47:45 +0900

    refactor: Move files to introduce feature to the application layer

--- lib/application/weather/usecases/reload_weather_usecase.dart
+++ lib/application/weather/usecases/reload_weather_usecase.dart
@@ -4,22 +4,25 @@ import 'package:flutter_training/domain/weather/entities/weather_info_entity.dar
 import 'package:flutter_training/domain/weather/entities/weather_target_entity.dart';
 import 'package:flutter_training/infrastructure/weather/repositories/weather_repository.dart';
 
-class ReloadWeatherUseCase implements UseCase<WeatherInfo> {
+class ReloadWeatherUseCase implements UseCase<WeatherInfoEntity> {
   const ReloadWeatherUseCase(this._weatherRepository);
 
   final WeatherRepository _weatherRepository;
 
   @override
   void execute({
-    required UseCaseSuccessCallback<WeatherInfo> onSuccess,
+    required UseCaseSuccessCallback<WeatherInfoEntity> onSuccess,
     required UseCaseErrorCallback onError,
   }) {
-    final weatherTarget = WeatherTarget(area: 'tokyo', date: DateTime.now());
+    final weatherTarget = WeatherTargetEntity(
+      area: 'tokyo',
+      date: DateTime.now(),
+    );
     final result = _weatherRepository.getWeather(weatherTarget);
     switch (result) {
-      case Success<WeatherInfo>():
+      case Success<WeatherInfoEntity>():
         onSuccess(result.value);
-      case Failure<WeatherInfo>():
+      case Failure<WeatherInfoEntity>():
         onError(result.message);
     }
   }
