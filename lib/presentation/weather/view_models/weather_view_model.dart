f476172d9a885a3e9a7e9ab52a0bdd7750231e6f
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-18 16:58:41 +0900

    chore: Add explanation of reloadWeather function

--- lib/presentation/weather/view_models/weather_view_model.dart
+++ lib/presentation/weather/view_models/weather_view_model.dart
@@ -11,6 +11,9 @@ class WeatherViewModel extends _$WeatherViewModel {
   @override
   WeatherViewState build() => const WeatherViewState();
 
+  // 天気予報を取得し、実行可否でstateを更新する
+  // - 成功時: 取得した内容で表示内容を更新する
+  // - 失敗時: message以外はstateを更新しない
   void reloadWeather() {
     final result = ref
         .read(reloadWeatherUseCaseProvider)
