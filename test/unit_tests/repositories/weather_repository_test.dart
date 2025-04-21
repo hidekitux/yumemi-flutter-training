47d637bcbfd82943d83f7717e46d5a7672e222aa
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-21 17:57:38 +0900

    test: Add unit tests for WeatherRepository

---
+++ test/unit_tests/repositories/weather_repository_test.dart
@@ -0,0 +1,98 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/domain/common/constants/common_error_messages.dart';
+import 'package:flutter_training/domain/common/interfaces/result.dart';
+import 'package:flutter_training/domain/weather/constants/weather_error_messages.dart';
+import 'package:flutter_training/domain/weather/entities/weather_info_entity.dart';
+import 'package:flutter_training/infrastructure/weather/api/weather_service.dart';
+import 'package:flutter_training/infrastructure/weather/repositories/weather_repository.dart';
+import 'package:flutter_training/infrastructure/weather/repository_providers.dart';
+import 'package:flutter_training/infrastructure/weather/service_providers.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+import '../../fixtures/weather_fixtures.dart';
+import 'weather_repository_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<WeatherService>()])
+void main() {
+  // Arrange
+  final mockWeatherService = MockWeatherService();
+  final weatherRepository = WeatherRepository(mockWeatherService);
+  final weatherTarget = createWeatherTarget();
+
+  test('weatherRepositoryProvider creates WeatherRepository', () {
+    // Arrange
+    final container = ProviderContainer(
+      overrides: [weatherServiceProvider.overrideWithValue(mockWeatherService)],
+    );
+
+    // Act
+    final repository = container.read(weatherRepositoryProvider);
+
+    // Assert
+    expect(repository, isA<WeatherRepository>());
+  });
+
+  test('getWeather returns expected weather info, if API call succeeds', () {
+    // Arrange
+    final expectedWeatherInfo = createWeatherInfo(date: weatherTarget.date);
+    when(mockWeatherService.fetchWeather(any)).thenReturn(expectedWeatherInfo);
+
+    // Act
+    final result = weatherRepository.getWeather(weatherTarget);
+
+    // Assert
+    expect(result, isA<Success<WeatherInfoEntity>>());
+    final weatherInfo = (result as Success<WeatherInfoEntity>).value;
+    expect(weatherInfo, equals(expectedWeatherInfo));
+    verify(mockWeatherService.fetchWeather(weatherTarget)).called(1);
+  });
+
+  for (final error in YumemiWeatherError.values) {
+    test('getWeather returns Failure, if API throws $error', () {
+      // Arrange
+      late Result<WeatherInfoEntity> result;
+      when(mockWeatherService.fetchWeather(any)).thenThrow(error);
+
+      // Act and Assert
+      expect(
+        () => result = weatherRepository.getWeather(weatherTarget),
+        returnsNormally,
+      );
+      expect(
+        result,
+        isA<Failure<WeatherInfoEntity>>().having(
+          (e) => (e as Failure).message,
+          'check message',
+          error.message,
+        ),
+      );
+      verify(mockWeatherService.fetchWeather(weatherTarget)).called(1);
+    });
+  }
+
+  test('getWeather returns Failure, if API returns invalid data', () {
+    // Arrange
+    late Result<WeatherInfoEntity> result;
+    when(
+      mockWeatherService.fetchWeather(any),
+    ).thenThrow(const FormatException());
+
+    // Act and Assert
+    expect(
+      () => result = weatherRepository.getWeather(weatherTarget),
+      returnsNormally,
+    );
+    expect(
+      result,
+      isA<Failure<WeatherInfoEntity>>().having(
+        (failure) => failure.message,
+        'check message',
+        CommonErrorMessages.unknown.message,
+      ),
+    );
+    verify(mockWeatherService.fetchWeather(weatherTarget)).called(1);
+  });
+}
