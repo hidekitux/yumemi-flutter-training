fec0e0653bd12e1689f74590a3d24c2340799ec0
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-23 16:09:15 +0900

    feat: Accept WeatherTargetEntity in reloadWeather

--- lib/presentation/weather/view_models/weather_view_model.dart
+++ lib/presentation/weather/view_models/weather_view_model.dart
@@ -14,10 +14,8 @@ class WeatherViewModel extends _$WeatherViewModel {
   /// 天気予報を取得し、実行結果に応じてstateを更新する
   /// - 成功時: エラーメッセージを消去し、取得した天気予報でstateを更新する
   /// - 失敗時: 元の天気予報を保持しながら、エラーメッセージでstateを更新する
-  void reloadWeather() {
-    final result = ref
-        .read(reloadWeatherUseCaseProvider)
-        .call(WeatherTargetEntity(area: 'Tokyo', date: DateTime.now()));
+  void reloadWeather(WeatherTargetEntity weatherTarget) {
+    final result = ref.read(reloadWeatherUseCaseProvider).call(weatherTarget);
 
     switch (result) {
       case Success(value: final weatherInfo):
