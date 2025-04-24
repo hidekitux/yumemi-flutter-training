21f234280db1b54f381924b8acfe152e72c7c4cd
Hideki Tanaka <hi_tanaka@yumemi.co.jp>
2025-04-24 14:53:26 +0900

    refactor: Use default date in weather fixtures to remove date requirement

--- test/fixtures/weather_fixtures.dart
+++ test/fixtures/weather_fixtures.dart
@@ -2,21 +2,21 @@ import 'package:flutter_training/domain/weather/constants/weather_condition.dart
 import 'package:flutter_training/domain/weather/entities/weather_info_entity.dart';
 import 'package:flutter_training/domain/weather/entities/weather_target_entity.dart';
 
+final defaultDateTime = DateTime(2025, 4);
+
 WeatherTargetEntity createWeatherTarget({
   String area = 'tokyo',
   DateTime? date,
-}) => WeatherTargetEntity(area: area, date: date ?? DateTime(2025, 4));
+}) => WeatherTargetEntity(area: area, date: date ?? defaultDateTime);
 
-/// createWeatherTargetを利用するテストにおいて、そのdateや他の値をdateに明示的に指定する
-/// createWeatherTargetを利用しないテストにおいて、nullや他の値をdateに明示的に指定する
 WeatherInfoEntity createWeatherInfo({
-  required DateTime? date,
   WeatherCondition? weatherCondition,
   int? maxTemperature,
   int? minTemperature,
+  DateTime? date,
 }) => WeatherInfoEntity(
   weatherCondition: weatherCondition ?? WeatherCondition.sunny,
   maxTemperature: maxTemperature ?? 25,
   minTemperature: minTemperature ?? 1,
-  date: date ?? DateTime(2025, 4),
+  date: date ?? defaultDateTime,
 );
